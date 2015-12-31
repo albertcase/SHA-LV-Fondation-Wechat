228c63c58d4b654fe47274639069d045b4dfceda
Vincen Wen <vincen.wen@samesamechina.com>
2015-12-31 12:50:40 +0800

    no message

--- protected/views/wechat/boulogne.php
+++ protected/views/wechat/boulogne.php
@@ -9,9 +9,7 @@
 				<img src="/wechat/imgs/boulogne.jpg" width="100%" />
 			</p>
 			<p>
-				林内道路纵横交错，树木郁郁葱葱，西边是向北流的塞纳河，东边是巴黎的富人区，北临最富庶的塞纳河畔讷伊(Neuilly sur Seine)，南靠布洛涅-比扬古(Boulogne Billancourt) 。<br />
-				森林北部有动植物公园（Jardin d'acclimatation）、民间艺术与传统博物馆（Musée des Arts et Traditions populaires），东北濒临马约门广场 (Place de la Porte de Maillot)，西北部有训练场（Champ d'entraînement）、运动场（Terrain de Sports）和巴加特勒公园 (Parc Bagatelle)，西南部有隆尚跑马场 (Hippodrome de Longchamp)。<br />
-				森林里有十几个湖和池塘，两个最大的湖是东部的南北长形湖，南边的叫“上湖”(Lac Supérieur) 长约400米，北边的叫“下湖” (Lac inférieur)，长1120多米，这个湖里面还有两个不小的岛。岛的西边有共和国骑兵卫队（Garde Républicaine à Cheval）营房和赛马俱乐部（Racing Club）。
+				<img src="/wechat/imgs/boulogne_text.png" width="100%" />
 			</p>
 		</div>
 
