7f07e0a29be37f83e9a374edf0450cbdd42a30aa
Vincen Wen <vincen.wen@samesamechina.com>
2016-01-04 11:16:21 +0800

    no message

--- protected/views/wechat/traffic.php
+++ protected/views/wechat/traffic.php
@@ -9,13 +9,13 @@
 			<h2>公共交通</h2>
 			<p class="ditie">
 				<span>地铁：</span><br />
-				地铁   <em>1</em>   号线 Les Sablons站 <br />
-				跟随 FONDATION LOUIS VUITTON标志步行
+				地铁   <em>1</em>   号线 Les Sablons 站 <br />
+				跟随 FONDATION LOUIS VUITTON 标志步行
 				<br />
 			</p>
 			<p class="gche">
 				<span>公车：</span><br />
-				<em>244</em>    路公交车FONDATION LOUIS VUITTON站<br />
+				<em>244</em>    路公交车 FONDATION LOUIS VUITTON 站<br />
 				只在周末运行<br />
 			</p>
 		</div>
@@ -28,10 +28,10 @@
 				基金会接驳车：1欧元
 			</p>
 			<p>
-				根据基金会开放时间发车，约每10至15分钟一趟。
+				根据基金会开放时间发车，约每 10 至 15 分钟一趟。
 			</p>
 			<p>
-				首班车在基金会开放前15分钟由戴高乐广场发出（Place Charles de Gaulle），发车点位于Avenue de Friedland 大街和地铁口交汇处；末班车在基金会关闭后由会址出发（星期二及基金会关闭日无车）。
+				首班车在基金会开放前 15 分钟由戴高乐广场发出（Place Charles de Gaulle），发车点位于 Avenue de Friedland 大街和地铁口交汇处；末班车在基金会关闭后由会址出发（星期二及基金会关闭日无车）。
 			</p>
 		</div>
 
