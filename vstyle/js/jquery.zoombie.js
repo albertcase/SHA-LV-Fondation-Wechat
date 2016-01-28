69b597736665d814f19465702ac6b21efbfd3c7a
Vincen Wen <vincen.wen@samesamechina.com>
2016-01-28 14:58:43 +0800

    no message

--- vstyle/js/jquery.zoombie.js
+++ vstyle/js/jquery.zoombie.js
@@ -38,36 +38,80 @@
             imageTag.onload = function(){
                 widthRatio = this.width / obj.width();
                 heightRatio = this.height / obj.height();
-            }
 
-            target.css({ backgroundImage: "url('" + imageSrc + "')" });
+                target.css({ backgroundImage: "url('" + imageSrc + "')" });
 
-            target.on("touchmove", setImage);
-            $(this).on("touchstart", setImage);
-            $(this).on("touchmove", setImage);
+                target.on("touchmove", setImage);
+                obj.on("touchstart", setImage);
+                obj.on("touchmove", setImage);
 
-            target.on("touchend", function(){
-                //target.hide();
-                //console.log(target.css("display"));
-                $(".introFooter").show();
-                if(target.css("display") != "none"){
-                    $(".narrowIcon").show();
-                }else{
-                    $(".zoomStatus").show();
-                }; 
-            });
-            $(this).on("touchend", function(){
-                //target.hide();
-                //console.log(target.css("display"));
-                $(".introFooter").show();
-                if(target.css("display") != "none"){
-                    $(".narrowIcon").show();
-                }else{
-                    $(".zoomStatus").show();
-                };
+                target.on("touchend", function(){
+                    //target.hide();
+                    //console.log(target.css("display"));
+                    $(".introFooter").show();
+                    if(target.css("display") != "none"){
+                        $(".narrowIcon").show();
+                    }else{
+                        $(".zoomStatus").show();
+                    }; 
+                });
+
+                obj.on("touchend", function(){
+                    //target.hide();
+                    //console.log(target.css("display"));
+                    $(".introFooter").show();
+                    if(target.css("display") != "none"){
+                        $(".narrowIcon").show();
+                    }else{
+                        $(".zoomStatus").show();
+                    };
+
+                    
+                });
+
+
+
+                function setImage(e) {
+                    $(".zoomtips").hide();
+                    $(".introFooter").hide();
+                    $(".narrowIcon").hide();
+
+                    var touch = e.originalEvent.changedTouches[0];
+
+                    var leftPos = parseInt(touch.pageX - offset.left);
+                    var topPos = parseInt(touch.pageY - offset.top);
+
+                    if (leftPos < 0 || topPos < 0 || leftPos > obj.width() || topPos > obj.height()) {
+                        target.hide();
+                        $(".zoomStatus").show();
+                    }else {
+                        target.show();
+
+                        $(".zoomStatus").hide();
+     
+                        leftPos = String(((touch.pageX - offset.left) * widthRatio - target.width() / 2) * (-1));
+                        topPos = String(((touch.pageY - offset.top) * heightRatio - target.height() / 2) * (-1));
+
+                        target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+                        // if(leftPos * (-1) >= 0 && leftPos * (-1) <= target.width()*2 - target.offset().left*2){
+                        //     target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+                        // }
+
+                        // if(leftPos * (-1) >= 0 && leftPos * (-1) <= target.width()*2 - target.offset().left*2 && topPos * (-1) >= 0 && topPos * (-1) <= target.height()){
+                        //     target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+                        // }
+                        
+                        leftPos = String(touch.pageX - target.width() / 2);
+                        topPos = String(touch.pageY - target.height() / 2 + parseInt(obj.css("margin-top")) - Math.floor(offset.top));
+                        target.css({ left: leftPos + 'px', top: topPos + 'px' });
+                        //target.css({ left: '50%', top: '50%', "margin-top": -parseInt(options.Size/2), "margin-left": -parseInt(options.Size/2) });
+                    }
+                }
 
                 
-            });
+
+            }
+
 
             $(".zoomStatus").on("touchstart", function(){
                 $(this).hide();
@@ -82,42 +126,6 @@
             // target.mousemove(setImage);
             // $(this).mousemove(setImage);
 
-            function setImage(e) {
-                $(".zoomtips").hide();
-                $(".introFooter").hide();
-                $(".narrowIcon").hide();
-
-            	var touch = e.originalEvent.changedTouches[0];
-
-                var leftPos = parseInt(touch.pageX - offset.left);
-                var topPos = parseInt(touch.pageY - offset.top);
-
-                if (leftPos < 0 || topPos < 0 || leftPos > obj.width() || topPos > obj.height()) {
-                    target.hide();
-                    $(".zoomStatus").show();
-                }else {
-                    target.show();
-
-                    $(".zoomStatus").hide();
- 
-                    leftPos = String(((touch.pageX - offset.left) * widthRatio - target.width() / 2) * (-1));
-                    topPos = String(((touch.pageY - offset.top) * heightRatio - target.height() / 2) * (-1));
-
-                    target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
-                    // if(leftPos * (-1) >= 0 && leftPos * (-1) <= target.width()*2 - target.offset().left*2){
-                    //     target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
-                    // }
-
-                    // if(leftPos * (-1) >= 0 && leftPos * (-1) <= target.width()*2 - target.offset().left*2 && topPos * (-1) >= 0 && topPos * (-1) <= target.height()){
-                    //     target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
-                    // }
-                    
-                    leftPos = String(touch.pageX - target.width() / 2);
-                    topPos = String(touch.pageY - target.height() / 2 + parseInt(obj.css("margin-top")) - Math.floor(offset.top));
-                    target.css({ left: leftPos + 'px', top: topPos + 'px' });
-                    //target.css({ left: '50%', top: '50%', "margin-top": -parseInt(options.Size/2), "margin-left": -parseInt(options.Size/2) });
-                }
-            }
 
             $(".backBtn").click(function(){
 
